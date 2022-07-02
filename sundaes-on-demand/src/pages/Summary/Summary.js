09888a0ae0c0bee811343d763c428eb1983ca5ba
exploitenomah <iksploiting@gmail.com>
2022-07-02 20:22:52 +0100

    set up folder structure for sundaes-on-demand

---
+++ sundaes-on-demand/src/pages/Summary/Summary.js
@@ -0,0 +1,15 @@
+
+import SummaryForm from './SummaryForm'
+
+
+const Summary = () => {
+
+  return (
+    <>
+     <h1>Order Summary</h1>
+      <SummaryForm />
+    </>
+  )
+}
+
+export default Summary
---
+++ sundaes-on-demand/src/pages/Summary/SummaryForm.js
@@ -0,0 +1,10 @@
+
+
+const SummaryForm = () => {
+
+  return(
+    <form>
+      
+    </form>
+  )
+}
