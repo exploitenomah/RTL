0b6991a2fd1e05b261f677d23dafefa1185caad0
exploitenomah <iksploiting@gmail.com>
2022-07-03 16:42:32 +0100

    write code to match specs for sundaes on demand

--- sundaes-on-demand/src/pages/Summary/SummaryForm.js
+++ sundaes-on-demand/src/pages/Summary/SummaryForm.js
@@ -3,7 +3,7 @@ import { useState } from 'react'
 const SummaryForm = () => {
 
   const [acceptTerms, setAcceptTerms] = useState(false)
- 
+  const [showPopover, setShowPopover] = useState(false)
    return(
      <form aria-label="place order"> 
         <input
@@ -11,9 +11,19 @@ const SummaryForm = () => {
         aria-checked={acceptTerms}
         checked={acceptTerms}
         aria-label='accept terms and conditons'
+        id='tc'
         onChange={(e) => setAcceptTerms(prev => !prev)}
        /> 
+       <label 
+       htmlFor='tc'
+       data-testid='terms-and-conditions'
+       onMouseOver={() => setShowPopover(true)}
+       onMouseOut={() => setShowPopover(false)}
+        >
+       accept terms and conditions
+       </label> 
        <button disabled={!acceptTerms}>Place Order</button>
+       {showPopover ? <p data-testid='tooltip'>there are no terms and conditions</p> : ''}
      </form> 
    )
  }
